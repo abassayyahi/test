f17fdbf0d0263b8d7123bd2cf6b470470f5ff571
abassayyahi <54246992+abassayyahi@users.noreply.github.com>
2023-09-23 18:44:56 +0330

    Update kucoin_api.py

--- apis/kucoin_api.py
+++ apis/kucoin_api.py
@@ -1,14 +1,14 @@
 from exchange_apis import ExchangeAPI
 
-
 class KuCoinAPI(ExchangeAPI):
     BASE_URL = "https://api.kucoin.com/api/v1"
 
-    # KuCoin-specific methods go here
-    def get_ticker(self, symbol):
+    def get_ticker(self, symbol: str) -> dict:
+        """Retrieve the ticker price for a given symbol from KuCoin."""
         endpoint = f"/market/orderbook/level1?symbol={symbol}"
         return self._send_request("GET", endpoint)
 
-    def get_fee_structure(self):
+    def get_fee_structure(self) -> dict:
+        """Retrieve the user info, which may include fee details, from KuCoin."""
         endpoint = "/user/info"
         return self._send_request("GET", endpoint)
