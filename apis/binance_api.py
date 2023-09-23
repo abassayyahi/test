c9a3a75b3c514741f2c3e06ab346910e48e851e0
abassayyahi <54246992+abassayyahi@users.noreply.github.com>
2023-09-23 18:44:15 +0330

    Update binance_api.py

--- apis/binance_api.py
+++ apis/binance_api.py
@@ -3,11 +3,12 @@ from exchange_apis import ExchangeAPI
 class BinanceAPI(ExchangeAPI):
     BASE_URL = "https://api.binance.com/api/v3"
 
-    # Binance-specific methods go here
-    def get_ticker(self, symbol):
+    def get_ticker(self, symbol: str) -> dict:
+        """Retrieve the ticker price for a given symbol from Binance."""
         endpoint = f"/ticker/price?symbol={symbol}"
         return self._send_request("GET", endpoint)
 
-    def get_fee_structure(self):
+    def get_fee_structure(self) -> dict:
+        """Retrieve the fee structure from Binance."""
         endpoint = "/exchangeInfo"
         return self._send_request("GET", endpoint)
