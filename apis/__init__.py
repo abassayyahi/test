4388b33a3983bfe6a91c10e5c5350b7c838b054f
abassayyahi <54246992+abassayyahi@users.noreply.github.com>
2023-09-23 18:43:52 +0330

    Update __init__.py

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,9 +1,6 @@
-from .kucoinn import KuCoinAPI
-from .binancee import BinanceAPI
-from .gateio import GateIOAPI
-from .mexc import MEXCAPI
-from .okex import OKexAPI
-from .bybit import BybitAPI
+from .kucoin_api import KuCoinAPI
+from .binance_api import BinanceAPI
+
 # ... other exchanges ...
 
-__all__ = ['KuCoinAPI', 'BinanceAPI', 'GateIOAPI', 'MEXCAPI', 'OKexAPI', 'BybitAPI']  # ... other exchanges ...
+__all__ = ['KuCoinAPI', 'BinanceAPI']  # ... other exchanges ...
